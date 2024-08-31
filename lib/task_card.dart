050046da07cb62d388731ac8d3c8d248ac280c26
ManaRegen <132016620+ManaRegen@users.noreply.github.com>
2024-08-31 11:25:06 -0700

    feat: implement frontend of upcoming_tasks_page

---
+++ lib/task_card.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'task.dart';
+
+class TaskCard extends StatefulWidget {
+  final Task task;
+
+  const TaskCard({
+    super.key,
+    required this.task,
+  });
+
+  @override
+  State<TaskCard> createState() => _TaskCardState();
+}
+
+class _TaskCardState extends State<TaskCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      color: const Color.fromARGB(255, 140, 84, 150),
+      elevation: 2.0,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Checkbox(
+              value: widget.task.isComplete,
+              onChanged: (bool? value) {
+                setState(() {
+                  widget.task.isComplete = value ?? false;
+                });
+              },
+            ),
+            Expanded(
+              child: Text(
+                widget.task.name,
+                style: TextStyle(
+                  color: Colors.white,
+                  decoration: widget.task.isComplete
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/upcoming_tasks_page.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'task_card.dart';
+import 'task.dart';
+import 'package:provider/provider.dart';
+import 'task_provider.dart';
+
+class UpcomingTasksPage extends StatelessWidget {
+  const UpcomingTasksPage({super.key});
+  final int daysInWeek = 7;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Upcoming Tasks"),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          double minListViewWidth = 300.0;
+          int numberOfColumns =
+              (constraints.maxWidth / minListViewWidth).floor();
+          numberOfColumns =
+              numberOfColumns > daysInWeek ? daysInWeek : numberOfColumns;
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Row(
+              children: List.generate(
+                numberOfColumns,
+                (index) {
+                  return TasksForDateColumn(index: index);
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// A single column in upcoming_tasks_page
+class TasksForDateColumn extends StatefulWidget {
+  final int index;
+
+  const TasksForDateColumn({
+    super.key,
+    required this.index,
+  });
+
+  @override
+  State<TasksForDateColumn> createState() => _TasksForDateColumnState();
+}
+
+class _TasksForDateColumnState extends State<TasksForDateColumn> {
+  late DateTime columnDate;
+  late String dayName;
+  late String formattedColumnDateStr;
+  late List<Task> tasksForDate; // need to calculate somehow
+
+  List<Task> _getTasksForDate(BuildContext context) {
+    final taskProvider = Provider.of<TaskProvider>(context, listen: false);
+    return taskProvider.tasks.where((task) {
+      return task.dueDate?.year == columnDate.year &&
+          task.dueDate?.month == columnDate.month &&
+          task.dueDate?.day == columnDate.day;
+    }).toList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    columnDate = DateTime.now().add(Duration(days: widget.index));
+    dayName = _getDayName(columnDate);
+    formattedColumnDateStr = "$dayName (${columnDate.month}/${columnDate.day})";
+    tasksForDate = _getTasksForDate(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Column(
+          children: [
+            const SizedBox(height: 12),
+            Text(formattedColumnDateStr),
+            const SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                itemCount: tasksForDate.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: TaskCard(task: tasksForDate[index]),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+String _getDayName(DateTime date) {
+  const List<String> dayNames = [
+    'Monday', // 1
+    'Tuesday', // 2
+    'Wednesday', // 3
+    'Thursday', // 4
+    'Friday', // 5
+    'Saturday', // 6
+    'Sunday', // 7
+  ];
+  return dayNames[date.weekday - 1];
+}
