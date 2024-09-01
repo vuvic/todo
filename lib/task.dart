4394694df5d7aa17e06b5eb1baaeda7c47b5bcd4
ManaRegen <132016620+ManaRegen@users.noreply.github.com>
2024-08-31 19:43:11 -0700

    feat: implement conversion between task object and map

--- lib/task.dart
+++ lib/task.dart
@@ -59,4 +59,25 @@ class Task {
   String toString() {
     return '$name: $description (Created on: $_creationTime, Due: ${dueDate ?? 'No due date'}, isComplete: $isComplete)';
   }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'creationTime': _creationTime,
+      'dueDate': dueDate,
+      'isComplete': isComplete ? 1 : 0,
+      'priority': priority,
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      id: map['id'],
+      name: map['name'],
+      dueDate: map['dueDate'],
+      isComplete: map['isComplete'] == 1,
+      priority: map['priority'],
+    );
+  }
 }
