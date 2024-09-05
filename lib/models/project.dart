acbf1f11d7d7e43e35c1f39969b2891c5fa9e21b
ManaRegen <132016620+ManaRegen@users.noreply.github.com>
2024-09-04 17:56:34 -0700

    feat: implement validation and encapsulation for Project.dart

--- lib/models/project.dart
+++ lib/models/project.dart
@@ -1,27 +1,68 @@
 import './task.dart';
 
 class Project {
+  int? _id;
   String _name;
-  List<Task>? _tasks;
-  List<Project>? _subProjects;
+  final List<Task> _tasks = [];
+  final List<Project> _subProjects = [];
+  final DateTime _creationTime;
 
-  Project(this._name, this._tasks, this._subProjects);
+  Project({
+    required String name,
+    int? id,
+  })  : _name = name,
+        _id = id,
+        _creationTime = DateTime.now();
 
+  DateTime get creationTime => _creationTime;
   String get name => _name;
+  int get id => id;
   List<Task>? get tasks => _tasks;
-  List<Project>? get subProjects => _subProjects;
 
-  void updateProject(
-      String? name, List<Task>? tasks, List<Project>? subProjects) {
-        if (name != null) {
-          _name = name;
-        }
-        if (tasks != null) {
-          _tasks = tasks;
-        }
-        if (subProjects != null) {
-          _subProjects = subProjects;
-        }
-      }
-  
+  set name(String value) {
+    if (name.isEmpty) {
+      throw ArgumentError("Name cannot be empty.");
+    }
+    _name = value;
+  }
+
+  void addTask(Task task) {
+    if (_tasks.contains(task)) {
+      throw ArgumentError(
+          "Task with _id '${task.id}' is already in the project.");
+    }
+    _tasks.add(task);
+  }
+
+  void removeTaskById(int id) {
+    _tasks.removeWhere((task) => task.id == id);
+  }
+
+  void addSubProject(Project subProject) {
+    if (this == subProject) {
+      throw ArgumentError("A project cannot be a subproject of itself.");
+    }
+
+    if (_hasCircularReference(subProject)) {
+      throw ArgumentError("Circular reference detected.");
+    }
+
+    if (_subProjects.contains(subProject)) {
+      throw ArgumentError("Subproject already exists.");
+    }
+
+    _subProjects.add(subProject);
+  }
+
+  void removeSubProjectByID(int id) {
+    _subProjects.removeWhere((subProject) => subProject._id == id);
+  }
+
+  bool _hasCircularReference(Project project) {
+    if (project == this) return true;
+    for (var sub in project._subProjects) {
+      if (_hasCircularReference(sub)) return true;
+    }
+    return false;
+  }
 }
